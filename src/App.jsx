6d406d760f7c765430827002438516a704e1c830
MacBook <macbook@Diosy.local>
2023-04-07 19:13:39 -0400

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import Home from './Routes/Home';
+import Detail from './Routes/Detail';
+import Favs from './Routes/Favs';
+import Contact from './Routes/Contact';
 
 function App() {
   return (
       <div className="App">
-          <Navbar/>
-          <Footer/>
+        <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Detail/>}/>
+          <Route path='/' element={<Favs/>}/>
+          <Route path='/' element={<Contact/>}/>
+        </Routes>
+        </BrowserRouter>
+        <Footer/>
       </div>
   );
 }
